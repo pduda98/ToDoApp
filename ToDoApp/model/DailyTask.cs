02e88c3dface2ed7187fa8eefaa3c9c634a9bd38
pduda98 <pduda.98@gmail.com>
2020-05-09 00:01:13 +0200

    nowe klasy

---
+++ ToDoApp/model/DailyTask.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.model
+{
+    class DailyTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int CategoryId { get; set; }
+        public DateTime Time { get; set; }
+        public bool Monday { get; set; }
+        public bool Tuesday { get; set; }
+        public bool Wednesday { get; set; }
+        public bool Thursday { get; set; }
+        public bool Friday { get; set; }
+        public bool Saturday { get; set; }
+        public bool Sunday { get; set; }
+        public bool IsImportant { get; set; }
+    }
+}
---
+++ ToDoApp/model/IntervalTask.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApp.model
+{
+    class IntervalTask
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description  { get; set; } 
+        public int CategoryId { get; set; }
+        public DateTime FirstDeadline { get; set; }
+        public TimeSpan Interval { get; set; }
+        public bool IsImportant { get; set; }
+    }
+}
